56d550ecac9bab4bf65eb6befefb486579695bd2
Aditya Kumar <adityak14200@gmail.com>
2023-03-24 14:10:46 -0400

    Final Version

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faP, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { EditTodoForm } from "./EditTodoForm";
 
-export const todo = () => {
+export const Todo = ({ task, toggleComplete, deleteTodo, editTodo}) => {
   return (
-    <div>todo</div>
-  )
-}
+    <div className="Todo">
+      <p
+        onClick={() => toggleComplete(task.id)}
+        className={`${task.completed ? "completed" : ""}`}
+      >
+        {task.task}
+      </p>
+      <div>
+        <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)}/>
+        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)}/>
+      </div>
+    </div>
+  );
+};
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,20 +1,27 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-export const TodoForm = ({addTodo}) => {
-    const [value, setValue] = useState("")
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
 
-    const handleSubmit = e => {
-      e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-      addTodo(value);
+    addTodo(value);
 
-      setValue("");
-    }
+    setValue("");
+  };
   return (
-    <form className='TodoForm' onSubmit={handleSubmit}>
-        <input type='text' className = 'todo-input' value = {value} placeholder='What is the task today?' onChange={(e) => setValue(e.target.value)}/>
-        <button type='submit' className='todo-btn'>Add Task
-        </button>
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={value}
+        placeholder="What is the task today?"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
     </form>
-  )
-}
+  );
+};
